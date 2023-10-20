82b464de440414b1aa83b73cb43d8281cfc30f36
Azra Eiden <carter.eiden@gmail.com>
2023-10-20 01:17:43 -0400

    Fix

--- rule/lambda_function.py
+++ rule/lambda_function.py
@@ -119,9 +119,9 @@ def lambda_handler(event, context):
         # If NOT retrying, and we are instead deleting, then we start with the DELETE call 
         #   (sometimes you start with GET STATE if you need to make a call for the identifier)
         elif event.get("op") == "delete":
+            eh.add_state({"name": prev_state["props"]["name"], "event_bus_name": prev_state["props"]["event_bus_name"]})
             eh.add_op("remove_targets_delete")
             eh.add_op("delete_rule")
-            eh.add_state({"name": prev_state["props"]["name"], "event_bus_name": prev_state["props"]["event_bus_name"]})
 
         # The ordering of call declarations should generally be in the following order
         # GET STATE
